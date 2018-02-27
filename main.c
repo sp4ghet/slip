a436f354b2fcd34db2b6c42a2799de3b69f9bcd3
Rikuo Hasegawa <rikuo.hase1997@gmail.com>
2018-02-27 21:38:24 +0900

    Q-expressions are implemented. Can now write macros/metaprogramming stuff, I think

--- main.c
+++ main.c
@@ -6,6 +6,9 @@
 
 #include "lib/mpc.h"
 
+#define LASSERT(args, cond, err)\
+  if(!(cond)){lval_del(args); return lval_err(err);}
+
 // create new language structure: lval
 // -----------------------------------
 
@@ -23,9 +26,19 @@ typedef struct lval {
 void lval_print(lval* v);
 lval* lval_eval_sexpr(lval* v);
 lval* builtin_op(lval* a, char* op);
+lval* builtin_head(lval* v);
+lval* builtin_tail(lval* v);
+lval* builtin_list(lval* v);
+lval* builtin_eval(lval* v);
 
 // types in our system
-enum {LVAL_ERR, LVAL_NUM, LVAL_SYM, LVAL_SEXPR};
+enum {
+  LVAL_ERR,
+  LVAL_NUM,
+  LVAL_SYM,
+  LVAL_SEXPR,
+  LVAL_QEXPR
+};
 
 // lval constructors
 // -----------------
@@ -60,6 +73,14 @@ lval* lval_sexpr(void){
   return v;
 }
 
+lval* lval_qexpr(void){
+  lval* v = malloc(sizeof(lval));
+  v->type = LVAL_QEXPR;
+  v->count = 0;
+  v->cell = NULL;
+  return v;
+}
+
 void lval_del(lval* v){
   switch(v->type){
     case LVAL_NUM: break;
@@ -67,14 +88,17 @@ void lval_del(lval* v){
     case LVAL_ERR: free(v->err); break;
     case LVAL_SYM: free(v->symbol); break;
 
+    case LVAL_QEXPR:
     case LVAL_SEXPR:
+      // free each cell from the cells array
       for(int i = 0; i < v->count; i++){
         lval_del(v->cell[i]);
       }
+      // free the cells array
       free(v->cell);
     break;
   }
-
+  // free the whole lvalue
   free(v);
 }
 
@@ -107,6 +131,7 @@ void lval_print(lval* v){
     case LVAL_SYM:
       printf("%s", v->symbol); break;
     case LVAL_SEXPR: lval_expr_print(v, '(', ')'); break;
+    case LVAL_QEXPR: lval_expr_print(v, '{', '}'); break;
   }
 }
 
@@ -132,10 +157,13 @@ lval* lval_read(mpc_ast_t* ast){
   lval* x = NULL;
   if(strcmp(ast->tag, ">") == 0) { x = lval_sexpr();}
   if(strstr(ast->tag, "sexpr")) {x = lval_sexpr();}
+  if(strstr(ast->tag, "qexpr")) {x = lval_qexpr();}
 
   for (int i = 0; i < ast->children_num; i++){
     if(strcmp(ast->children[i]->contents, "(") == 0){ continue; }
     if(strcmp(ast->children[i]->contents, ")") == 0){ continue; }
+    if(strcmp(ast->children[i]->contents, "{") == 0){ continue; }
+    if(strcmp(ast->children[i]->contents, "}") == 0){ continue; }
     if(strcmp(ast->children[i]->tag, "regex")  == 0){ continue; }
     x = lval_add(x, lval_read(ast->children[i]));
   }
@@ -183,7 +211,19 @@ lval* lval_eval_sexpr(lval* v){
     return lval_err("S-expression should start with a symbol!");
   }
 
-  lval* result = builtin_op(v, f->symbol);
+  // symbol (operator)
+  lval* result;
+  if(strstr(f->symbol, "head")){
+    result = builtin_head(v);
+  }else if(strstr(f->symbol, "tail")){
+    result = builtin_tail(v);
+  } else if(strstr(f->symbol, "list")){
+    result = builtin_list(v);
+  } else if(strstr(f->symbol, "eval")){
+    result = builtin_eval(v);
+  } else {
+    result = builtin_op(v, f->symbol);
+  }
   lval_del(f);
   return result;
 }
@@ -222,6 +262,54 @@ lval* builtin_op(lval* a, char* op){
   lval_del(a); return x;
 }
 
+lval* builtin_head(lval* v){
+  LASSERT(v, v->count == 1,
+    "Too many arguments passed to `head`, expected: 1");
+  LASSERT(v, v->cell[0]->type == LVAL_QEXPR,
+    "`head` expects input of type QEXPR");
+  LASSERT(v, v->cell[0]->count != 0,
+    "`head` was passed list {}, `head` is undefined for {}");
+
+  lval* argument_qexpr = lval_take(v, 0);
+  // unsure why I can't just lval_take(argument_qexpr, 0)
+  while(argument_qexpr->count > 1){ lval_del(lval_pop(argument_qexpr, 1)); }
+  return argument_qexpr;
+}
+
+lval* builtin_tail(lval* v){
+  LASSERT(v, v->count == 1,
+    "Too many arguments passed to `tail`, expected: 1");
+  LASSERT(v, v->cell[0]->type == LVAL_QEXPR,
+    "`tail` expects input of type QEXPR");
+  LASSERT(v, v->cell[0]->count != 0,
+    "`tail` was passed list {}, `tail` is undefined for {}");
+
+  lval* list = lval_take(v, 0);
+  lval_del(lval_pop(list, 0));
+  return list;
+}
+
+// takes an S-expression and returns the contents as a Q-expression
+// list(s...) -> q(s...)
+lval* builtin_list(lval* v){
+  v->type = LVAL_QEXPR;
+  return v;
+}
+
+// takes a qexpression that is not {} and returns the contents evalutated
+lval* builtin_eval(lval* v){
+  LASSERT(v, v->count == 1,
+    "Too many arguments passed to `eval`");
+  LASSERT(v, v->cell[0]->type == LVAL_QEXPR,
+    "Argument passed to `eval` should be Q-expression");
+  LASSERT(v, v->cell[0]->count != 0,
+    "`eval` was passed {}, need non-empty Q-expression");
+
+  lval* x = lval_take(v, 0);
+  x->type = LVAL_SEXPR;
+  return lval_eval(x);
+}
+
 // main loop
 // ---------
 
@@ -230,6 +318,7 @@ int main(int argc, char** argv){
     mpc_parser_t* Number    = mpc_new("number");
     mpc_parser_t* Symbol    = mpc_new("symbol");
     mpc_parser_t* SExpr     = mpc_new("sexpr");
+    mpc_parser_t* QExpr     = mpc_new("qexpr");
     mpc_parser_t* Expr      = mpc_new("expr");
     mpc_parser_t* Slip      = mpc_new("slip");
 
@@ -237,12 +326,15 @@ int main(int argc, char** argv){
     mpca_lang(MPCA_LANG_DEFAULT,
             "                                               \
             number  : /-?[0-9]+/;                           \
-            symbol  : '+' | '-' | '*' | '/';                \
+            symbol  : \"list\" | \"head\" | \"tail\" |      \
+                      \"eval\" | \"join\" |                 \
+                      '+' | '-' | '*' | '/';                \
             sexpr   : '(' <expr>* ')';                      \
-            expr    : <number> | <symbol> | <sexpr>;          \
+            qexpr   : '{' <expr>* '}';                      \
+            expr    : <number> | <symbol> | <sexpr> | <qexpr>;\
             slip    : /^/ <expr>* /$/;                      \
             ",
-            Number, Symbol, SExpr, Expr, Slip);
+            Number, Symbol, SExpr, QExpr, Expr, Slip);
 
     puts("Slip version 0.0.0.0");
     puts("Press ctrl+c to exit");
@@ -275,7 +367,7 @@ int main(int argc, char** argv){
         free(input);
     }
 
-    mpc_cleanup(4, Number, Symbol, SExpr, Expr, Slip);
+    mpc_cleanup(4, Number, Symbol, SExpr, QExpr, Expr, Slip);
 
     return 0;
 }
